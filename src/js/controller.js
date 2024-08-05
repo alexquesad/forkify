75fc1c78d0dc813936adc5925f54d77aa0b5579b
alexquesad <alexandraquesad@gmail.com>
2024-08-05 15:23:20 +0200

    started implementing search results

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,14 +1,9 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js'
+import searchView from './views/searchView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
-
-const recipeContainer = document.querySelector('.recipe');
-
-// https://forkify-api.herokuapp.com/v2
-
-///////////////////////////////////////
 
 const controlRecipes = async function (){
   try {
@@ -29,7 +24,21 @@ const controlRecipes = async function (){
   }
 };
 
+const controlSearchResults = async function(){
+  try{
+    const query = searchView.getQuery();
+    if (!query)
+      return;
+    await model.loadSearchResults(query);
+  }
+  catch(err){
+    console.log(err);
+  }
+
+}
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
 }
 init();
