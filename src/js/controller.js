77c92f7835a1343b2db15901571c8d2d8c2a5664
alexquesad <alexandraquesad@gmail.com>
2024-08-07 14:00:56 +0200

    Created a DOM updating Algorithm

--- src/js/controller.js
+++ src/js/controller.js
@@ -56,8 +56,16 @@ const controlPagination = function (goToPage){
   paginationView.render(model.state.search);
 }
 
+const controlServings = function(newServings) {
+  //update the recipe servings (in state)
+  model.updateServings(newServings);
+  //Update the recipe view
+  recipeView.update(model.state.recipe);
+}
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  recipeView.addHandlerUpdateServings(controlServings);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
 }
